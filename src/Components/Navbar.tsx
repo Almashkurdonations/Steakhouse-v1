c3c4bc6c79742486652b19e56e217281b8c222f3
Almashkurdonations <163398684+Almashkurdonations@users.noreply.github.com>
2026-08-09 14:08:46 +0100

    Update Navbar.tsx

---
+++ src/Components/Navbar.tsx
@@ -0,0 +1,15 @@
+function Navbar() {
+  return (
+    <nav className="navbar">
+      <div>STEAKHOUSE</div>
+
+      <div className="nav-links">
+        <a href="#">Menu</a>
+        <a href="#">Story</a>
+        <a href="#">Reservation</a>
+      </div>
+    </nav>
+  );
+}
+
+export default Navbar;
