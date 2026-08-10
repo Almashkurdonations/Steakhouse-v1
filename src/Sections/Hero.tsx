70d210408f20462046db2f29fbd42ab917ef3c69
Almashkurdonations <163398684+Almashkurdonations@users.noreply.github.com>
2026-08-10 14:21:18 +0100

    Update Hero.tsx

--- src/Sections/Hero.tsx
+++ src/Sections/Hero.tsx
@@ -1,35 +1,33 @@
-import Navbar from "../components/Navbar";
-
 function Hero() {
   return (
-    <section className="hero-wrapper">
+    <section className="hero">
+      <nav>
+        <h2>STEAKHOUSE</h2>
 
-      <div className="hero-sticky">
-
-        <Navbar />
-
-        <div className="hero-content">
-
-          <p className="subtitle">
-            Premium Wood Fired Dining
-          </p>
-
-          <h1>
-            STEAKHOUSE
-          </h1>
-
-          <button>
-            Reserve Table
-          </button>
-
+        <div>
+          <a>Menu</a>
+          <a>Story</a>
+          <a>Reservation</a>
         </div>
+      </nav>
 
-        <div className="scroll">
-          Scroll
-        </div>
+      <div className="hero-content">
+        <p>Premium Dining Experience</p>
 
+        <h1>
+          THE
+          <br />
+          STEAKHOUSE
+        </h1>
+
+        <button>
+          Reserve Table
+        </button>
       </div>
 
+      <div className="scroll">
+        Scroll
+      </div>
     </section>
   );
 }
