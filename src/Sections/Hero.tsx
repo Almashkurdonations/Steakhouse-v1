c0fc178b6bc65dd519cded1619ec41e0b6c9ee11
Almashkurdonations <163398684+Almashkurdonations@users.noreply.github.com>
2026-08-11 05:18:08 +0100

    Update Hero.tsx

--- src/Sections/Hero.tsx
+++ src/Sections/Hero.tsx
@@ -1,33 +1,60 @@
 function Hero() {
   return (
-    <section className="hero">
-      <nav>
-        <h2>STEAKHOUSE</h2>
+    <section className="hero-wrapper">
 
-        <div>
-          <a>Menu</a>
-          <a>Story</a>
-          <a>Reservation</a>
+      <div className="hero-sticky">
+
+        <video
+          className="hero-video"
+          src="/videos/hero-video.mp4"
+          autoPlay
+          muted
+          loop
+          playsInline
+        />
+
+        <div className="overlay" />
+
+
+        <nav className="navbar">
+
+          <h2>STEAKHOUSE</h2>
+
+          <div className="nav-links">
+            <a>Menu</a>
+            <a>Story</a>
+            <a>Reserve</a>
+          </div>
+
+        </nav>
+
+
+        <div className="hero-content">
+
+          <p>
+            Premium Dining Experience
+          </p>
+
+          <h1>
+            THE
+            <br />
+            STEAKHOUSE
+          </h1>
+
+          <button>
+            Reserve Table
+          </button>
+
         </div>
-      </nav>
 
-      <div className="hero-content">
-        <p>Premium Dining Experience</p>
 
-        <h1>
-          THE
-          <br />
-          STEAKHOUSE
-        </h1>
+        <div className="scroll">
+          Scroll
+        </div>
 
-        <button>
-          Reserve Table
-        </button>
+
       </div>
 
-      <div className="scroll">
-        Scroll
-      </div>
     </section>
   );
 }
